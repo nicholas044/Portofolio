b23336ce2ca79ea80273cc1888094f31870c43cf
Ainembabazi Nicholas <97317648+nicholas044@users.noreply.github.com>
2022-07-31 11:58:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Freelancer"],
+        strings: ["Digital Marketer","Content Creator","Graphic Designer","Web Developer", "Web Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Freelancer"],
+        strings: ["Digital Marketer","Content Creator","Graphic Designer","Web Developer", "Web Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
